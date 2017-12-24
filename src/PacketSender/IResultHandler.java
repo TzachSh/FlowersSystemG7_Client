ad7ab2344f8266f2a8784efeccbbd67c024af392
VladimirB <vovchik.balagula@gmail.com>
2017-12-24 21:48:13 +0200

    w

--- src/PacketSender/IResultHandler.java
+++ src/PacketSender/IResultHandler.java
@@ -1,5 +1,5 @@
 package PacketSender;
-//fqewq
+//fqewqwq
 /**
  * Interface that clients can use for implements their work when the result was arrived from the server
  * and also when waiting for result
