3c601cb73192f968b7d11dc098588fc3ef6d93c0
VladimirB <vovchik.balagula@gmail.com>
2017-12-29 21:57:13 +0200

    nothing

--- src/Products/ProductsUI.java
+++ src/Products/ProductsUI.java
@@ -2,9 +2,13 @@ package Products;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ProductsUI extends Application {
+
 
 	public static void main(String[] args) {
 		launch(args);
