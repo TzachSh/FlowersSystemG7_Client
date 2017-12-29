21c1ec082c560989431391b1c2ef37142b71123f
eliasnijim <33372570+eliasnijim@users.noreply.github.com>
2017-12-29 22:29:51 +0200

    added customer

---
+++ src/Customers/CustomerUI.java
@@ -0,0 +1,18 @@
+package Customers;
+
+import Products.CatalogProductController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CustomerUI extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		CustomerController customerController = new CustomerController();
+		customerController.start(primaryStage);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
