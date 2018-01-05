bfed878a2711ffae80d0b650607538ab054ebc6d
eliasnijim <33372570+eliasnijim@users.noreply.github.com>
2018-01-05 19:16:03 +0200

    d

--- src/Customers/UpdateCustomerController.java
+++ src/Customers/UpdateCustomerController.java
@@ -13,50 +13,48 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-public class UpdateCustomerController implements Initializable {
+class UpdateCustomerController implements Initializable{
 
-	
-	private User user;
-	public  UpdateCustomerController(User user) {
+	public UpdateCustomerController(User user) {
+		super();
 		// TODO Auto-generated constructor stub
-		this.user=user;
 	}
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
 	}
 	public void start(Stage primaryStage)  throws Exception{
-		// TODO Auto-generated method stub
-		String title = "Update Customer Information";
-		String srcFXML = "/Customers/updateCustomer.fxml";
-		String srcCSS = "/Customers/application.css";
 		
+			String title = "Update Customer Information";
+			String srcFXML = "/Customers/updateCustomerUI.fxml";
+			String srcCSS = "/Customers/application.css";
+			
 
-	
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource(srcFXML));
-			Parent root = loader.load();
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource(srcCSS).toExternalForm());
-			primaryStage.setTitle(title);
-			primaryStage.setScene(scene);
-			
-			primaryStage.show();
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e);
-			e.printStackTrace();
-		}
 		
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			
-			@Override
-			public void handle(WindowEvent event) {
-				// TODO Auto-generated method stub
-				Platform.exit();
+			try {
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(getClass().getResource(srcFXML));
+				Parent root = loader.load();
+				Scene scene = new Scene(root);
+				scene.getStylesheets().add(getClass().getResource(srcCSS).toExternalForm());
+				primaryStage.setTitle(title);
+				primaryStage.setScene(scene);
+				
+				primaryStage.show();
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e);
+				e.printStackTrace();
 			}
-		});
+			
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				
+				@Override
+				public void handle(WindowEvent event) {
+					// TODO Auto-generated method stub
+					Platform.exit();
+				}
+			});
 	}
 }
