73da96c5b726d4e0af1e30b10da59b18890cd87b
eliasnijim <33372570+eliasnijim@users.noreply.github.com>
2018-01-20 20:59:24 +0200

    added comments to membershipaccount.java

--- src/Customers/MemberShipAccount.java
+++ src/Customers/MemberShipAccount.java
@@ -4,34 +4,64 @@ import java.io.Serializable;
 import java.sql.Date;
 
 public class MemberShipAccount implements Serializable{
-	
+	private int acNum;
+	private int mId;
+	private Date creationDate;
+	/**
+	 * membership account constructor
+	 * @param acNum account number
+	 * @param mId membership id
+	 * @param creationDate creation date
+	 */
 	public MemberShipAccount(int acNum, int mId, Date creationDate) {
 		super();
 		this.acNum = acNum;
 		this.mId = mId;
 		this.creationDate = creationDate;
 	}
+	/**
+	 * 
+	 * @return account number
+	 */
 	public int getAcNum() {
 		return acNum;
 	}
+	/**
+	 * 
+	 * @param acNum setting account number
+	 */
 	public void setAcNum(int acNum) {
 		this.acNum = acNum;
 	}
+	/**
+	 * 
+	 * @return membership number
+	 */
 	public int getmId() {
 		return mId;
 	}
+	/**
+	 * 
+	 * @param mId setting membership number
+	 */
 	public void setmId(int mId) {
 		this.mId = mId;
 	}
+	/**
+	 * 
+	 * @return membership's creation date
+	 */
 	public Date getCreationDate() {
 		return creationDate;
 	}
+	/**
+	 * 
+	 * @param creationDate setting membership's creation date
+	 */
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
-	private int acNum;
-	private int mId;
-	private Date creationDate;
+
 	
 	
 
